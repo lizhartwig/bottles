23373b85ad8c821a8f687d5705a35f94d1088390
Elizabeth Hartwig <lizhartwig@yahoo.com>
2015-10-23 01:13:36 -0700

    make spec 2 pass

--- spec/specs.js
+++ spec/specs.js
@@ -2,5 +2,9 @@ describe('bottles', function () {
   it ("will only accept integers between 0 and 99", function () {
     expect(bottles(0)).to.equal(true);
   });
+  it ("will not accept strings", function () {
+    expect(bottles("are")).to.equal(false);
+  });
+
 
 });
