84f115040f485a152c8dc3ddc025172a817c90c1
Elizabeth Hartwig <lizhartwig@yahoo.com>
2015-10-23 01:49:04 -0700

    add more inputs and outputs

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,11 @@
 describe('bottles', function () {
-  it ("will only accept integers between 0 and 99", function () {
+  it("will only accept integers between 0 and 99", function () {
     expect(bottles(0)).to.equal(true);
+    expect(bottles(99)).to.equal(true);
+    expect(bottles(-1)).to.equal(false);
+    expect(bottles(100)).to.equal(false);
   });
-  it ("will not accept strings", function () {
+  it("will not accept strings", function () {
     expect(bottles("are")).to.equal(false);
   });
 
