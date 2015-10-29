5767c7c3351a49c37fdfe8a93d36f71f8144b5c7
Elizabeth Hartwig <lizhartwig@yahoo.com>
2015-10-29 00:13:55 -0700

    change code from boolean to string

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 var bottles = function(input) {
-  if ((input <= 99) && (input >= 0)){
-    return true;
-  } else {
-  return false;
+  if (input >= 2) {
+    return "99 bottles of beer on the wall, 99 bottles of beer.";
+  }   else if (input === 1) {
+    return "1 bottle of beer on the wall, 1 bottle of beer.";
+  }   else if (input === 0) {
+    return "No more bottles of beer on the wall, no more bottles of beer.";
+  }   else {
+    return "Please enter a valid number.";
   }
-};
+}
+
+// else {
+//     return "Please enter a valid number.")
+//   }
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,16 @@
-describe('bottles', function () {
-  it("will only accept integers between 0 and 99", function () {
-    expect(bottles(0)).to.equal(true);
-    expect(bottles(99)).to.equal(true);
-    expect(bottles(-1)).to.equal(false);
-    expect(bottles(100)).to.equal(false);
+describe('bottles', function() {
+  it("displays the lyrics to 99 bottles of the beer on the wall, if the user enters any number above '2'", function() {
+    expect(bottles(99)).to.equal("99 bottles of beer on the wall, 99 bottles of beer.");
   });
-  it("will not accept strings", function () {
-    expect(bottles("are")).to.equal(false);
+  it("displays the lyrics to 1 bottle of beer on the wall, if the user enters the number '1'", function() {
+    expect(bottles(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer.");
   });
-
+  it("displays the lyrics to 0 bottles of beer on the wall, if the user enters the number '0'", function() {
+    expect(bottles(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer.");
+  });
+    it("displays 'please enter a valid number', if the user enters anything other than than zero or positive numbers", function() {
+    expect(bottles("wing")).to.equal("Please enter a valid number.");
+  });
 
 });
+//   
